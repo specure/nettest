7f4f5249fcc3330fa9c145f4f27d124c6d75dbea
olsemeno <sendtosemenov@gmail.com>
2025-08-27 18:51:15 +0400

    123

--- src/tokio_server/server_config.rs
+++ src/tokio_server/server_config.rs
@@ -5,7 +5,7 @@ use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::fs;
 use std::io::BufReader;
-use std::net::{IpAddr, Ipv6Addr, SocketAddr};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
 use std::sync::Arc;
 use tokio_rustls::rustls::pki_types::{CertificateDer, PrivateKeyDer};
 use tokio_rustls::rustls::ServerConfig;
