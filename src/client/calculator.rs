231ccab323e77382490a8733a45e7677659d1827
olsemeno <sendtosemenov@gmail.com>
2026-02-24 16:55:41 +0400

    deb

--- src/client/calculator.rs
+++ src/client/calculator.rs
@@ -2,6 +2,7 @@ use crate::client::{client::Measurement};
 use log::debug;
 
 pub fn calculate_speed_from_measurements(measurements: Vec<Vec<(u64, u64)>>) -> (f64, f64, f64) {
+    log::debug!("calculate_speed_from_measurements measurements: {:?}", measurements);
     if measurements.is_empty() {
         println!("calculate_speed_from_measurements measurements is empty");
         return (0.0, 0.0, 0.0);
